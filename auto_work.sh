a6ac61dab2ff08833cf34d9a286c43d2e06ff23b
linuxwyn <fmccsgo007@outlook.com>
2017-11-27 19:22:01 +0800

    自动下载你所需要的源码包，并且编译链接成可执行文件，安装到你的计算机上
    因为编译器（例如gcc、cc等）和库文件的缺失，可能导致编译链接异常。出现此问题时仔细排查本地计算机是否缺失前面提到的依赖组件

--- auto_work.sh
+++ auto_work.sh
@@ -4,5 +4,5 @@ tar -zvcf $2      #利用tar解压你所下载的源码包
 cd $2*            #进入解压后的目录
 ./configure --prefix=/usr/local/$2 --conf-path=/etc/$2/$2".conf"          #配置安装目录和配置文件目录，这里可以自行更改
 make              #make会去查找makefile文件，利用系统的编译链接工具执行编译和链接，这里要注意可能有些库文件缺失导致出错
-make clean        #清除以前的旧的项目
+make clean        #清除以前的可能存在的旧项目
 make install      #执行安装，OK。接下来就是一写配置文件的更改了
