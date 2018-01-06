f28faf88d60c15d5065c948db6c696a64f5ac617
linuxwyn <fmccsgo007@outlook.com>
2018-01-06 23:29:22 +0800

    利用bc程序进行数制转换
    Attention
    1、有些*uix并没有bc这个程序
    2、obase必须写在ibase前面，不然你可以试试将ibase写在obase前面，然后将一个二进制数转换成十进制，你会发现得到的还是原来的那个二进制数

--- Number_system_convert.sh
+++ Number_system_convert.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "scale=6;obase=$2;ibase=$1;$3" | bc        #scale指精度，ibase指的是你输入数字的进制，同理，obase指的你想得到的进制
+if [ ! -x $0 ]
+then
+  chmod u+x $0
+fi
+echo "scale=6;obase=$2;ibase=$1;$3" | bc        #scale指精度，ibase指的是你输入数字的进制，同字面意思，obase指的你想得到的进制
+ 
